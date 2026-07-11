ff0d43d5a46a330abf2672ee66d648579c12bf4c
ThisaruG <thisaru.guruge@gmail.com>
2026-07-11 22:48:19 +0530

    test: add tests for file actions and ignore handler

---
+++ internal/engine/file_action_test.go
@@ -0,0 +1,598 @@
+/*
+All Rights Reversed (ɔ)
+*/
+
+package engine
+
+import (
+	"log/slog"
+	"os"
+	"slices"
+	"strings"
+	"testing"
+)
+
+type subTest struct {
+	name      string
+	fs        *MockFileSystem
+	want      []ActionEvent
+	wantErr   bool
+	wantErrIs error
+}
+
+func TestFileAction_execute(t *testing.T) {
+	tests := []struct {
+		name   string
+		action func(l *slog.Logger) fileAction
+		cases  []subTest
+	}{
+		{
+			name: "up-to-date",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionUpToDate("src", "dest", "reason", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{EventType: EventIgnore},
+					},
+				},
+			},
+		},
+		{
+			name: "skip",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionSkip("src", "dest", "reason", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionSkip,
+							Msg:       "src -> dest [reason]",
+							EventType: EventSkip,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "link",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionLink("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventSuccess,
+						},
+					},
+				},
+				{
+					name: "fail",
+					fs: &MockFileSystem{
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "replace",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionReplace("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventStep,
+						},
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventStep,
+						},
+						{
+							Action:    actionRemove,
+							Msg:       "dest.bestow.tmp",
+							EventType: EventSuccess,
+						},
+					},
+				},
+				{
+					name: "move fail",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - move pass",
+					fs: &MockFileSystem{
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - recovery fail",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							if strings.Contains(src, "tmp") {
+								return os.ErrPermission
+							}
+							return nil
+						},
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      nil,
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "tmp remove fail",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							if strings.Contains(path, "tmp") {
+								return os.ErrPermission
+							}
+							return nil
+						},
+					},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventStep,
+						},
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventStep,
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "backup",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionBackup("src", "dest", "dest.bestow.backup", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionBackup,
+							Msg:       "dest -> dest.bestow.backup",
+							EventType: EventStep,
+						},
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventSuccess,
+						},
+					},
+				},
+				{
+					name: "move fails",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - recovery fail",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							if strings.Contains(src, "backup") {
+								return os.ErrPermission
+							}
+							return nil
+						},
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want: []ActionEvent{
+						{
+							Action:    actionBackup,
+							Msg:       "dest -> dest.bestow.backup",
+							EventType: EventStep,
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - recovery pass",
+					fs: &MockFileSystem{
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "adopt",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionAdopt("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionAdopt,
+							Msg:       "dest -> src",
+							EventType: EventStep,
+						},
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventSuccess,
+						},
+					},
+				},
+				{
+					name: "move fails",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - recovery fail",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							if strings.Contains(src, "src") {
+								return os.ErrPermission
+							}
+							return nil
+						},
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want: []ActionEvent{
+						{
+							Action:    actionAdopt,
+							Msg:       "dest -> src",
+							EventType: EventStep,
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "link fail - recovery pass",
+					fs: &MockFileSystem{
+						linkFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "remove",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionRemove("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventSuccess,
+						},
+					},
+				},
+				{
+					name: "remove fail",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							return os.ErrPermission
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fa := tc.action(newTestLogger())
+			for _, st := range tc.cases {
+				t.Run(st.name, func(t *testing.T) {
+					events, err := fa.execute(st.fs)
+					if st.want != nil {
+						if !slices.Equal(events, st.want) {
+							t.Fatalf("got %v, want %v", events, st.want)
+						}
+					}
+					validateErrScenario(t, st.wantErr, err, st.wantErrIs)
+				})
+			}
+		})
+	}
+}
+
+func TestFileAction_undo(t *testing.T) {
+	tests := []struct {
+		name   string
+		action func(l *slog.Logger) fileAction
+		cases  []subTest
+	}{
+		{
+			name: "up-to-date",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionUpToDate("src", "dest", "reason", l)
+			},
+			cases: []subTest{
+				{
+					name: "run",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{EventType: EventIgnore},
+					},
+				},
+			},
+		},
+		{
+			name: "skip",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionSkip("src", "dest", "reason", l)
+			},
+			cases: []subTest{
+				{
+					name: "run",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{EventType: EventIgnore},
+					},
+				},
+			},
+		},
+		{
+			name: "link",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionLink("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "link file",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							return nil
+						},
+					},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventUndo,
+						},
+					},
+				},
+				{
+					name: "remove fail",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							return os.ErrPermission
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "replace",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionReplace("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventUndo,
+						},
+					},
+				},
+				{
+					name: "remove fail",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "backup",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionBackup("src", "dest", "dest.bestow.backup", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionRestore,
+							Msg:       "dest.bestow.backup -> dest",
+							EventType: EventUndo,
+						},
+					},
+				},
+				{
+					name: "move fails",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "adopt",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionAdopt("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventUndo,
+						},
+						{
+							Action:    actionRestore,
+							Msg:       "src -> dest",
+							EventType: EventUndo,
+						},
+					},
+				},
+				{
+					name: "remove fails",
+					fs: &MockFileSystem{
+						removeFn: func(path string) error {
+							return os.ErrPermission
+						},
+					},
+					want:      []ActionEvent{},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+				{
+					name: "move fails",
+					fs: &MockFileSystem{
+						moveFn: func(src, target string) error {
+							return os.ErrPermission
+						},
+					},
+					want: []ActionEvent{
+						{
+							Action:    actionRemove,
+							Msg:       "dest",
+							EventType: EventUndo,
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+		{
+			name: "remove",
+			action: func(l *slog.Logger) fileAction {
+				return newFileActionRemove("src", "dest", l)
+			},
+			cases: []subTest{
+				{
+					name: "no errors",
+					fs:   &MockFileSystem{},
+					want: []ActionEvent{
+						{
+							Action:    actionLink,
+							Msg:       "dest -> src",
+							EventType: EventUndo,
+						},
+					},
+				},
+				{
+					name: "link fail",
+					fs: &MockFileSystem{
+						linkFn: func(src, dest string) error {
+							return os.ErrPermission
+						},
+					},
+					wantErr:   true,
+					wantErrIs: os.ErrPermission,
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fa := tc.action(newTestLogger())
+			for _, st := range tc.cases {
+				t.Run(st.name, func(t *testing.T) {
+					events, err := fa.undo(st.fs)
+					if st.want != nil {
+						if !slices.Equal(events, st.want) {
+							t.Fatalf("got %v, want %v", events, st.want)
+						}
+					}
+					validateErrScenario(t, st.wantErr, err, st.wantErrIs)
+				})
+			}
+		})
+	}
+}
---
+++ internal/engine/ignore_handler_test.go
@@ -0,0 +1,107 @@
+/*
+All Rights Reversed (ɔ)
+*/
+
+package engine
+
+import (
+	"os"
+	"slices"
+	"testing"
+)
+
+type mockIgnoreReader struct {
+	existsFn    func(path string) (bool, error)
+	readLinesFn func(path string) ([]string, error)
+}
+
+func (m *mockIgnoreReader) Exists(path string) (bool, error) {
+	if m.existsFn != nil {
+		return m.existsFn(path)
+	}
+	return false, nil
+}
+
+func (m *mockIgnoreReader) ReadLines(path string) ([]string, error) {
+	if m.readLinesFn != nil {
+		return m.readLinesFn(path)
+	}
+	return nil, nil
+}
+
+func TestReadIgnoreFile(t *testing.T) {
+	tests := []struct {
+		name      string
+		reader    *mockIgnoreReader
+		want      []string
+		wantErr   bool
+		wantErrIs error
+	}{
+		{
+			name: "file not exist",
+			reader: &mockIgnoreReader{
+				existsFn: func(path string) (bool, error) {
+					return false, nil
+				},
+			},
+		},
+		{
+			name: "file read error",
+			reader: &mockIgnoreReader{
+				existsFn: func(path string) (bool, error) {
+					return false, os.ErrPermission
+				},
+			},
+			wantErr:   true,
+			wantErrIs: os.ErrPermission,
+		},
+		{
+			name: "file with no comments",
+			reader: &mockIgnoreReader{
+				existsFn: func(path string) (bool, error) {
+					return true, nil
+				},
+				readLinesFn: func(path string) ([]string, error) {
+					return []string{".git"}, nil
+				},
+			},
+			want: []string{".git"},
+		},
+		{
+			name: "file with comments",
+			reader: &mockIgnoreReader{
+				existsFn: func(path string) (bool, error) {
+					return true, nil
+				},
+				readLinesFn: func(path string) ([]string, error) {
+					return []string{".git", "  # This is a comment", "     nvim", "# Another Comment", "this line is not # Comment"}, nil
+				},
+			},
+			want: []string{".git", "nvim", "this line is not # Comment"},
+		},
+		{
+			name: "read lines error",
+			reader: &mockIgnoreReader{
+				existsFn: func(path string) (bool, error) {
+					return true, nil
+				},
+				readLinesFn: func(path string) ([]string, error) {
+					return nil, os.ErrPermission
+				},
+			},
+			wantErr:   true,
+			wantErrIs: os.ErrPermission,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			lines, err := readIgnoreFile("file_path", tc.reader)
+			if validateErrScenario(t, tc.wantErr, err, tc.wantErrIs) {
+				return
+			}
+			if !slices.Equal(lines, tc.want) {
+				t.Fatalf("got %v, want %v", lines, tc.want)
+			}
+		})
+	}
+}
