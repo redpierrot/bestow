9c9f180756187a489eea2a23f0051643a44e1b13
ThisaruG <thisaru.guruge@gmail.com>
2026-07-13 23:26:26 +0530

    test: add tests for undoFileAction function

--- internal/engine/engine_test.go
+++ internal/engine/engine_test.go
@@ -5,6 +5,8 @@ All Rights Reversed (ɔ)
 package engine
 
 import (
+	"os"
+	"slices"
 	"testing"
 )
 
@@ -19,17 +21,201 @@ func TestEngine_executeFileActions(t *testing.T) {
 
 func TestEngine_undoFileActions(t *testing.T) {
 	tests := []struct {
-		name      string
-		input     *Summary
-		want      *Summary
-		action    fileAction
-		isUndo    bool
-		wantErr   bool
-		wantErrIs error
-	}{}
+		name        string
+		fs          *mockFileSystem
+		actions     []fileAction
+		events      []ActionEvent
+		wantEvents  []ActionEvent
+		wantSummary Summary
+		wantErr     bool
+		wantErrIs   error
+	}{
+		{
+			name: "one event undo: skip",
+			actions: []fileAction{
+				newFileActionSkip("src", "dest", "reason", newTestLogger()),
+			},
+			events: []ActionEvent{
+				{
+					Action:    actionSkip,
+					Msg:       "src -> dest [reason]",
+					EventType: EventSkip,
+				},
+			},
+			wantEvents: []ActionEvent{
+				{
+					Action:    actionSkip,
+					Msg:       "src -> dest [reason]",
+					EventType: EventSkip,
+				},
+				{
+					EventType: EventIgnore,
+				},
+			},
+		},
+		{
+			name: "one event undo: link",
+			actions: []fileAction{
+				newFileActionLink("src", "dest", newTestLogger()),
+			},
+			fs: &mockFileSystem{},
+			events: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src -> dest",
+					EventType: EventSuccess,
+				},
+			},
+			wantEvents: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src -> dest",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionRemove,
+					Msg:       "dest",
+					EventType: EventUndo,
+				},
+			},
+			wantSummary: Summary{
+				reverted: 1,
+			},
+		},
+		{
+			name: "multiple events",
+			actions: []fileAction{
+				newFileActionLink("src1", "dest1", newTestLogger()),
+				newFileActionLink("src2", "dest2", newTestLogger()),
+				newFileActionLink("src3", "dest3", newTestLogger()),
+			},
+			fs: &mockFileSystem{},
+			events: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src1 -> dest1",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src2 -> dest2",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src3 -> dest3",
+					EventType: EventSuccess,
+				},
+			},
+			wantEvents: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src1 -> dest1",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src2 -> dest2",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src3 -> dest3",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionRemove,
+					Msg:       "dest3",
+					EventType: EventUndo,
+				},
+				{
+					Action:    actionRemove,
+					Msg:       "dest2",
+					EventType: EventUndo,
+				},
+				{
+					Action:    actionRemove,
+					Msg:       "dest1",
+					EventType: EventUndo,
+				},
+			},
+			wantSummary: Summary{
+				reverted: 3,
+			},
+		},
+		{
+			name: "multiple events - undo fail in middle",
+			actions: []fileAction{
+				newFileActionLink("src1", "dest1", newTestLogger()),
+				newFileActionLink("src2", "dest2", newTestLogger()),
+				newFileActionLink("src3", "dest3", newTestLogger()),
+			},
+			fs: &mockFileSystem{
+				removeFn: func(path string) error {
+					if path == "dest2" {
+						return os.ErrPermission
+					}
+					return nil
+				},
+			},
+			events: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src1 -> dest1",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src2 -> dest2",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src3 -> dest3",
+					EventType: EventSuccess,
+				},
+			},
+			wantEvents: []ActionEvent{
+				{
+					Action:    actionLink,
+					Msg:       "src1 -> dest1",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src2 -> dest2",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionLink,
+					Msg:       "src3 -> dest3",
+					EventType: EventSuccess,
+				},
+				{
+					Action:    actionRemove,
+					Msg:       "dest3",
+					EventType: EventUndo,
+				},
+			},
+			wantSummary: Summary{
+				reverted: 1,
+			},
+			wantErr:   true,
+			wantErrIs: os.ErrPermission,
+		},
+	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-
+			summary := Summary{}
+			e := newTestEngine(tc.fs, nil)
+			result, err := e.undoFileActions(tc.actions, &summary, tc.events)
+			validateErrScenario(t, tc.wantErr, err, tc.wantErrIs)
+			if !slices.Equal(result.Events, tc.wantEvents) {
+				t.Fatalf("got %v, want %v", result.Events, tc.wantEvents)
+			}
+			if summary != tc.wantSummary {
+				t.Fatalf("got %v, want %v", summary, tc.wantSummary)
+			}
 		})
 	}
 }
@@ -45,67 +231,58 @@ func TestEngine_updateSummary(t *testing.T) {
 		{
 			name:    "up-to-date",
 			summary: Summary{},
-			want: Summary{
-				UpToDate: 1,
-			},
-			action: newFileActionUpToDate("", "", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionUpToDate: 1}},
+			action:  newFileActionUpToDate("", "", "", newTestLogger()),
 		},
 		{
 			name:    "skip",
 			summary: Summary{},
-			want: Summary{
-				Skipped: 1,
-			},
-			action: newFileActionSkip("", "", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionSkip: 1}},
+			action:  newFileActionSkip("", "", "", newTestLogger()),
 		},
 		{
 			name:    "link",
 			summary: Summary{},
-			want: Summary{
-				Stowed: 1,
-			},
-			action: newFileActionLink("", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionLink: 1}},
+			action:  newFileActionLink("", "", newTestLogger()),
 		},
 		{
 			name:    "replace",
 			summary: Summary{},
-			want: Summary{
-				Replaced: 1,
-			},
-			action: newFileActionReplace("", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionReplace: 1}},
+			action:  newFileActionReplace("", "", newTestLogger()),
 		},
 		{
 			name:    "backup",
 			summary: Summary{},
-			want: Summary{
-				BackedUp: 1,
-			},
-			action: newFileActionBackup("", "", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionBackup: 1}},
+			action:  newFileActionBackup("", "", "", newTestLogger()),
 		},
 		{
 			name:    "adopt",
 			summary: Summary{},
-			want: Summary{
-				Adopted: 1,
-			},
-			action: newFileActionAdopt("", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionAdopt: 1}},
+			action:  newFileActionAdopt("", "", newTestLogger()),
 		},
 		{
 			name:    "remove",
 			summary: Summary{},
-			want: Summary{
-				Unstowed: 1,
-			},
-			action: newFileActionRemove("", "", newTestLogger()),
+			want:    Summary{counts: [numActionKinds]int{ActionRemove: 1}},
+			action:  newFileActionRemove("", "", newTestLogger()),
 		},
 		{
 			name:    "undo",
 			summary: Summary{},
 			isUndo:  true,
-			want: Summary{
-				Reverted: 1,
-			},
-			action: newFileActionRemove("", "", newTestLogger()),
+			want:    Summary{reverted: 1},
+			action:  newFileActionRemove("", "", newTestLogger()),
+		},
+		{
+			name:    "undo skip",
+			summary: Summary{},
+			isUndo:  true,
+			want:    Summary{},
+			action:  newFileActionSkip("", "", "", newTestLogger()),
 		},
 	}
 	for _, tc := range tests {
